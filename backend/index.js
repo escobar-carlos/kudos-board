830a3105279ac24c07da4cc201ee605249d40d53
Carlos Escobar <carlos27@meta.com>
2025-06-18 11:16:35 -0700

    remove loading PORT from env for Render purposes

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,20 @@
 const cors = require('cors');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT;
+// const PORT = process.env.PORT;
+const PORT = 5432;
 
 // const { PrismaClient } = require('@prisma/client');
 // const prisma = new PrismaClient();
 
-// const boards = require('./routes/boards');
-// const cards = require('./routes/cards');
+const boards = require('./routes/boards');
+const cards = require('./routes/cards');
 
 app.use(cors());
 app.use(express.json());
 
-// app.use('/boards', boards);
-// app.use('/boards/:boardId/cards', cards);
+app.use('/boards', boards);
+app.use('/boards/:boardId/cards', cards);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
